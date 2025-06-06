53cdcac2c6a14ca2caf53140b09e37b580d9f2b1
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-06-05 22:26:15 -0700

    dependencies and placeholders

---
+++ components/links.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import { PenIcon } from "lucide-react";
+
+type Links = {
+    title: string;
+    thumbnail: string;
+    link: string;
+    icon: ReactNode;
+};
+
+const links: Links[] = [
+    {
+        title: "Instagram",
+        thumbnail: "https://www.instagram.com/aacodee/?hl=en",
+        link: "https://www.instagram.com/aacodee/?hl=en",
+        icon: <PenIcon />,
+    }
+];
