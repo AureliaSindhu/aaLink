35634c83b5b8f33765573cfde03ce743822d46de
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-07-07 13:00:38 -0700

    update: header

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,17 @@
+import { Link } from "lucide-react";
+
 export default function Header() {
     return (
         <header className="flex flex-col items-center justify-center gap-4 mt-16">
             <div className="flex items-center gap-2 bg-blue-500/20 backdrop-blur-lg border border-blue-500/30 px-4 py-2 rounded-full">
-            <h2 className="text-2xl text-[var(--blue)] font-bold">aacode</h2>
-            <div className="w-2.5 h-2.5 rounded-full bg-green-400 animate-pulse"></div>
+                <h2 className="text-2xl text-[var(--blue)] font-bold">aaLink</h2>
+                <div className="w-2.5 h-2.5 rounded-full bg-green-400 animate-pulse"></div>
             </div>
+
+            <p className="flex flex-row text-sm text-gray-500">
+                <Link className="pr-1" size={18} /> aaLink is a collection of links to my social media and fun projects
+            </p>
+
         </header>
     );
 }
