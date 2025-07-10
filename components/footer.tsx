fe32151263f2926e0d68098e1a009be81f78c4d0
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-07-09 21:43:34 -0700

    feat: shareable

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="w-full py-2 text-center text-sm text-neutral-500 fixed bottom-0">
+        <footer className="w-full py-2 mb-2 text-center text-sm text-neutral-500 fixed bottom-0">
             <p>
                 &copy; {new Date().getFullYear()} made by {" "}
                 <a href="https://www.instagram.com/aacodee/?hl=en" target="_blank" rel="noopener noreferrer" className="font-semibold hover:underline text-blue-400">
--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,31 @@
+"use client";
+
 import { Link } from "lucide-react";
+import { toast } from "sonner";
 
 export default function Header() {
     return (
-        <header className="flex flex-col items-center justify-center gap-4 mt-16 px-4">
+        <header className="flex flex-col items-center justify-center gap-4 mt-12 px-4">
             <div className="flex items-center gap-2 bg-blue-500/20 backdrop-blur-lg border border-blue-500/30 px-4 py-2 rounded-full">
                 <h2 className="text-xl md:text-2xl text-[var(--blue)] font-bold">aaLink</h2>
                 <div className="w-2.5 h-2.5 rounded-full bg-green-400 animate-pulse"></div>
             </div>
 
             <p className="flex flex-row text-xs md:text-sm text-gray-500 text-center">
-                {/* <Link className="pr-1" size={18} />  */}
-                aaLink is a collection of links to my social media and fun projects
+                aacode's personal linktree
+                <button
+                    onClick={() => {
+                        const url = window.location.href;
+                        navigator.clipboard.writeText(url);
+                        toast.success("Link copied to clipboard", {
+                            duration: 2000,
+                            position: 'bottom-center'
+                        });
+                    }}
+                    className="flex items-center text-gray-500 hover:text-gray-700 transition-colors ml-2"
+                >   
+                    <Link size={14} />
+                </button>
             </p>
 
         </header>
